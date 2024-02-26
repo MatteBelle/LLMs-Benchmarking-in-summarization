fa3a0c97b54254b8579f6f2ebcaf31f062f6c3f6
Pastour97 <parsa.pmk.code@gmail.com>
2024-02-26 17:33:34 +0100

    updated bart_score with the right max_length

--- bart_score.py
+++ bart_score.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 class BARTScorer:
-    def __init__(self, device='cuda:0', max_length=1024, checkpoint='facebook/bart-large-cnn'):
+    def __init__(self, device='cuda:0', max_length=4096, checkpoint='facebook/bart-large-cnn'):
         # Set up model
         self.device = device
         self.max_length = max_length
